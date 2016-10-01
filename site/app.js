c5b89f29834773e41487ff6549922d4741778bad
Dustin J Eckroth <eckroth.dj@gmail.com>
2016-10-01 12:02:16 +0900

    Hi

---
+++ site/app.js
@@ -0,0 +1,73 @@
+(function () {
+  'use strict';
+
+  angular.module('ShoppingListCheckOff', [])
+         .controller('ToBuyController', ToBuyController)
+         .controller('AlreadyBoughtController', AlreadyBoughtController)
+         .service('ShoppingListCheckOffService', ShoppingListCheckOffService);
+
+  ToBuyController.$inject = ['ShoppingListCheckOffService'];
+  AlreadyBoughtController.$inject = ['ShoppingListCheckOffService'];
+
+  function ToBuyController(ShoppingListCheckOffService) {
+    var toBuyList = this;
+
+    toBuyList.items = ShoppingListCheckOffService.getToBuyItems();
+    toBuyList.message = "This list is empty!";
+    toBuyList.newItemName = "";
+    toBuyList.newItemQuantity = "";
+
+    toBuyList.moveToBoughtList = function(index) {
+      ShoppingListCheckOffService.moveToBoughtList(index);
+    };
+
+    toBuyList.addItem = function() {
+      var item = { name: toBuyList.newItemName,
+                   quantity: toBuyList.newItemQuantity };
+
+      ShoppingListCheckOffService.addItem(item);
+    }
+  }
+
+  function AlreadyBoughtController(ShoppingListCheckOffService) {
+      var boughtList = this;
+
+      boughtList.items = ShoppingListCheckOffService.getBoughtItems();
+      boughtList.message = "This list is empty!";
+  }
+
+  function ShoppingListCheckOffService() {
+    var service = this;
+
+    var toBuyListItems = [
+              { name: "cookies", quantity: 10 },
+              { name: "chips", quantity: 2 },
+              { name: "breads", quantity: 5 },
+              { name: "fruits", quantity: 6 },
+              { name: "veggies", quantity: 3 }];
+
+    var boughtListItems = [];
+
+    service.moveToBoughtList = function(index) {
+      var item = toBuyListItems[index];
+      //console.log(toBuyListItems[index]);
+
+      boughtListItems.push(item);
+      toBuyListItems.splice(index,1);
+    };
+
+    service.getToBuyItems = function() {
+      return toBuyListItems;
+    };
+
+    service.getBoughtItems = function() {
+      return boughtListItems;
+    };
+
+    service.addItem = function(item) {
+      toBuyListItems.push(item);
+    }
+  }
+
+
+})(); // iife end
